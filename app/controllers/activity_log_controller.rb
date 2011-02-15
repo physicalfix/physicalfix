c26ee126631c7707338389a823873cb2fd2efb13
Ryan Jafari <ryan.jafari@gmail.com>
2011-02-15 13:27:30 -0500

    activity log hotfix

--- app/controllers/activity_log_controller.rb
+++ app/controllers/activity_log_controller.rb
@@ -20,6 +20,8 @@ class ActivityLogController < ApplicationController
     else
       if @ua.errors.on(:duration)
         flash[:error] = "Please enter a valid duration"
+      elsif @ua.errors.on(:activity)
+        flash[:error] = "Please select an activity"
       end
     end
     redirect_to activity_log_index_path
@@ -43,46 +45,46 @@ class ActivityLogController < ApplicationController
   end
 
   private
-  def find_activities_by_date
-    if params[:id]
-      @date = Time.parse(params[:id])
-    else
-      @date = Time.now
-    end
-
-    @activities = current_user.user_activities.all(:order => 'activity_date ASC', :conditions => ['activity_date BETWEEN ? and ?', @date.beginning_of_day.utc, @date.end_of_day.utc])
-
-    @date = @date.to_date
-    calc_activity_info
-  end
-  
-  def calc_activity_info
-    @activity_duration_total = @activities.inject(0) do |s,activity|
-      
-      if activity.duration
-        s += activity.duration
+    def find_activities_by_date
+      if params[:id]
+        @date = Time.parse(params[:id])
       else
-        s += 0
+        @date = Time.now
       end
+
+      @activities = current_user.user_activities.all(:order => 'activity_date ASC', :conditions => ['activity_date BETWEEN ? and ?', @date.beginning_of_day.utc, @date.end_of_day.utc])
+
+      @date = @date.to_date
+      calc_activity_info
     end
 
-    @total_calories = @activities.inject(0) do |s, activity|
-      s += activity.calories
-    end
+    def calc_activity_info
+      @activity_duration_total = @activities.inject(0) do |s,activity|
 
-    @activity_calorie_total = @activities.inject(0) do |s, activity|
-      if activity.name != 'Daily Energy Expenditure'
+        if activity.duration
+          s += activity.duration
+        else
+          s += 0
+        end
+      end
+
+      @total_calories = @activities.inject(0) do |s, activity|
         s += activity.calories
-      else
-        s += 0
+      end
+
+      @activity_calorie_total = @activities.inject(0) do |s, activity|
+        if activity.name != 'Daily Energy Expenditure'
+          s += activity.calories
+        else
+          s += 0
+        end
       end
     end
-  end
-  
-  def setup
-    @activities_list = Activity.all(:order => 'name ASC')
-    @user_activity = UserActivity.new
-    @user = current_user
-    @favorites = current_user.favorite_activities
-  end
+
+    def setup
+      @activities_list = Activity.all(:order => 'name ASC')
+      @user_activity = UserActivity.new
+      @user = current_user
+      @favorites = current_user.favorite_activities
+    end
 end
