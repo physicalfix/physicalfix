674bcacf508a2cff3e5e5f8edc0a6bd37f2063be
Ryan Jafari <ryan.jafari@gmail.com>
2011-02-14 23:21:11 -0500

    fixed

--- app/helpers/workout_helper.rb
+++ app/helpers/workout_helper.rb
@@ -22,7 +22,17 @@ module WorkoutHelper
 
   def trial_week_number(day_of_week, start_date, end_date)
     days = days_of_week_between(day_of_week, start_date, end_date)
-    return days == 0 ? 1 : days
+    case days
+    when 1
+      if (start_date == end_date) && (start_date.wday == day_of_week) && (end_date.wday == day_of_week)
+        return 1
+      else
+        return 2
+      end
+    else
+      days += 1
+    end
+    return days
   end
 
   # Make sure dates passed are actually dates.
