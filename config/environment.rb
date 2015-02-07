231a2b98307a32c746f37907f018ed5ae97967ef
nikhil <admin@complitech.net>
2015-02-07 15:06:24 +0530

    commented metric_fu gems and its dependency

--- config/environment.rb
+++ config/environment.rb
@@ -44,9 +44,9 @@ Rails::Initializer.run do |config|
   # If you include more than the below, you get:
   # A copy of NewRelic::Agent::Agent has been removed from the module tree but is still active! (ArgumentError)
   # config.gem 'rspec', :version => '1.3.1'
-  config.gem 'rest-client', :version => '0.9.2'
+  #config.gem 'rest-client', :version => '0.9.2'
   config.gem 'haml', :version => '3.0.9'
-  config.gem 'chronic', :version => '0.2.3'
+  #config.gem 'chronic', :version => '0.2.3'
   config.gem 'whenever', :version => '0.4.2'
   config.gem 'httparty', :version => '0.5.2'
   config.gem 'raspell', :version => '1.1'
@@ -54,22 +54,22 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate', :version => '2.3.14'
   config.gem 'sunspot_rails', :version => '1.1.0', :lib => 'sunspot/rails'
 
-  config.gem 'saikuro', :version => '1.1.0'
+  #config.gem 'saikuro', :version => '1.1.0'
   config.gem 'rake', :version => '0.8.3'
   config.gem 'rcov', :version => '1.0.0'
   config.gem 'right_aws', :version => '2.0.0'
   config.gem 'mysql', :version => '2.9.1'
-  config.gem 'flog', :version => '2.2.0'
-  config.gem 'flay', :version => '2.6.1'
-  config.gem 'sexp_processor', :version => '3.0.7'
+  #config.gem 'flog', :version => '2.2.0'
+  #config.gem 'flay', :version => '2.6.1'
+  #config.gem 'sexp_processor', :version => '3.0.7'
   config.gem 'i18n', :version => '0.6.11'
-  config.gem 'chronic', :version => '0.6.2'
-  config.gem 'churn', :version => '0.0.15'
-  config.gem 'crack', :version => '0.1.6'
-  config.gem 'mime-types', :version => '1.16'
+  #config.gem 'chronic', :version => '0.6.2'
+  #config.gem 'churn', :version => '0.0.15'
+  #config.gem 'crack', :version => '0.1.6'
+  #config.gem 'mime-types', :version => '1.16'
   config.gem 'mongo_mapper' # updated this from 0.7.6 to 0.8.6 for use with new mongo db
   config.gem 'rake', :version => '0.8.3'
-  config.gem 'metric_fu', :version => '1.3.0'
+  #config.gem 'metric_fu', :version => '1.3.0'
 
   # config.gem 'aws-s3', :version => "0.6.2" # don't include aws-s3 it crashes startup. probably loaded elsewhere
   # config.gem 'god', :version => '0.10.1'
