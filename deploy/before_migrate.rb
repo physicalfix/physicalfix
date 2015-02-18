5945ea8a4f25b27ce5ce9bd626f8556cd40c0730
nikhil <admin@complitech.net>
2015-02-18 15:41:12 +0530

    remove warnings

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,7 +1,7 @@
-run "rm #{release_path}/config/initializers/mongo_config.rb"
-run "ln -nfs #{shared_path}/config/mongo_config.rb   #{release_path}/config/initializers/mongo_config.rb"
-run "rm #{release_path}/config/s3.yml"
-run "ln -nfs #{shared_path}/config/s3.yml #{release_path}/config/s3.yml"
+run "rm #{config.release_path}/config/initializers/mongo_config.rb"
+run "ln -nfs #{config.shared_path}/config/mongo_config.rb   #{config.release_path}/config/initializers/mongo_config.rb"
+run "rm #{config.release_path}/config/s3.yml"
+run "ln -nfs #{config.shared_path}/config/s3.yml #{config.release_path}/config/s3.yml"
 
 on_app_master do 
   sudo "monit unmonitor delayed_job"
