f2db556f6f1105b5993ca477e97de04227e11571
nikhil <admin@complitech.net>
2015-02-18 15:36:31 +0530

    s3 yml softlink added

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,5 +1,7 @@
 run "rm #{release_path}/config/initializers/mongo_config.rb"
 run "ln -nfs #{shared_path}/config/mongo_config.rb   #{release_path}/config/initializers/mongo_config.rb"
+run "rm #{release_path}/config/s3.yml"
+run "ln -nfs #{shared_path}/config/s3.yml #{release_path}/config/s3.yml"
 
 on_app_master do 
   sudo "monit unmonitor delayed_job"
